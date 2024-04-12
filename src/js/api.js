2196ec72ea33bd0f417514df35bf4ae330fdbed8
AlexBilka <aleksandrbelka@gmail.com>
2024-04-13 01:03:31 +0300

    add getReview function

--- src/js/api.js
+++ src/js/api.js
@@ -2,17 +2,36 @@ import axios from 'axios';
 import iziToast from 'izitoast';
 
 export async function postComment(email, comment) {
-  const post = { email, comment };
+  const inputData = { email, comment };
   try {
-    const res = await axios.post(
+    await axios.post(
       'https://portfolio-js.b.goit.study/api/requests',
-      post
+      inputData
     );
-    return res.data;
+    iziToast.info({
+      message: 'Thank you for your comment!)',
+      position: 'topRight',
+    });
+    return;
   } catch (err) {
     iziToast.info({
-      message: 'Please, add comment more 7 symbols and input valid email.',
+      message:
+        'Please add your comment more than 7 characters and enter a valid email.',
       position: 'topLeft',
+    });
+  }
+}
+
+export async function getReviews() {
+  try {
+    const res = await axios.get(
+      'https://portfolio-js.b.goit.study/api/requests'
+    );
+    return res;
+  } catch (err) {
+    iziToast.info({
+      message: 'Sorry, there are no reviews here(',
+      position: 'topRight',
     });
   }
 }
