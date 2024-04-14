e4068c68cf2fa508eaf6709fc89d2bafc6c72f2d
AlexBilka <aleksandrbelka@gmail.com>
2024-04-14 23:07:42 +0300

    add swiper

---
+++ src/js/about-me.js
@@ -0,0 +1,47 @@
+import { getSwiper } from './swiper';
+
+getSwiper({
+  //   modules: [Navigation, Keyboard],
+  //   speed: 800,
+  //   grabCursor: true,
+  //   allowTouchMove: true,
+  //   direction: 'horizontal',
+  //   watchOverflow: true,
+  spaceBetween: 16,
+});
+
+// getSwiper({
+//   modules: [Navigation, Keyboard],
+//   speed: 800,
+//   grabCursor: true,
+//   allowTouchMove: true,
+//   direction: 'horizontal',
+//   watchOverflow: true,
+//   spaceBetween: 16,
+
+//   navigation: {
+//     nextEl: '.next-review-btn',
+//     prevEl: '.prev-review-btn',
+//   },
+
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: true,
+//     pageUpDown: true,
+//   },
+
+//   breakpoints: {
+//     320: {
+//       slidesPerGroup: 1,
+//       slidesPerView: 1,
+//     },
+//     768: {
+//       slidesPerGroup: 2,
+//       slidesPerView: 2,
+//     },
+//     1440: {
+//       slidesPerGroup: 4,
+//       slidesPerView: 4,
+//     },
+//   },
+// });
