4a3116b0b0cb5e4f659987d1858f97ab2e173bb2
AlexBilka <aleksandrbelka@gmail.com>
2024-04-15 23:47:10 +0300

    clean all markUp and conect working swiper

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import './js/api';
-import './js/acordeon';
+// import './js/api';
+// import './js/acordeon';
 import './js/swiper';
-import './js/footer';
-import './js/mobile-menu';
-import './js/projects';
-import './js/reviews';
+// import './js/footer';
+// import './js/mobile-menu';
+// import './js/projects';
+// import './js/reviews';
+import './js/about-me';
