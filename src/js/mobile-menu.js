4fcee4e04e59949419ae00b345ef67b484e96e78
Anton Postnov <4qc7@proton.me>
2024-04-15 16:44:56 +0300

    Update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,9 +2,9 @@ const menuCloseButton = document.getElementById('mobile-menu-close');
 const menuOpenButton = document.getElementById('mobile-menu-open');
 const mainModalMenuOpenButton = document.getElementById('main-modal-menu-open');
 const menu = document.getElementById('mobile-menu-id');
-const menuLink = document.getElementsByClassName('mobile-menu-nav-list-item');
+const menuLink = document.querySelectorAll('mobile-menu-nav-list-item');
 const mainModalMenu = document.getElementById('main-modal-menu-id');
-const mainModalMenuLink = document.getElementById('main-modal-list-item');
+const mainModalMenuLink = document.querySelectorAll('main-modal-list-item');
 const body = document.getElementsByTagName('body');
 
 // show/hide the menu when the button is clicked
@@ -14,8 +14,6 @@ for (let link of menuLink) {
     console.log("clicked link");
     menu.classList.remove('is-open');
     body[0].classList.remove('no-scroll');
-    console.log(menu);
-    console.log(body);
   });
 }
 
