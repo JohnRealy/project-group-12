de68a1b617d61e2f5b2417c0a8fd8fc687a134ee
Anton Postnov <4qc7@proton.me>
2024-04-15 19:46:07 +0300

    Update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -6,6 +6,8 @@ const menuLink = document.querySelectorAll('mobile-menu-nav-list-item');
 const mainModalMenu = document.getElementById('main-modal-menu-id');
 const mainModalMenuLink = document.querySelectorAll('main-modal-list-item');
 const body = document.getElementsByTagName('body');
+const mainMenu = document.querySelector('.main-modal-menu');
+
 
 // show/hide the menu when the button is clicked
 for (let link of menuLink) {
@@ -34,14 +36,31 @@ menuCloseButton.addEventListener('click', () => {
   body[0].classList.remove('no-scroll');
 });
 
-mainModalMenuOpenButton.addEventListener('click', () => {
-  mainModalMenu.classList.add('is-open2');
-});
+mainModalMenuOpenButton.addEventListener('click', handleClick);
 
+function handleClick() {
+  mainModalMenuOpenButton.addEventListener('click', remove);
+  addOpen();
+  mainModalMenuOpenButton.removeEventListener('click', handleClick);
+}
+
+function addOpen() {
+  mainMenu.classList.add('is-open2');
+}
+
+function remove() {
+  mainMenu.classList.remove('is-open2');
+  mainModalMenuOpenButton.addEventListener('click', handleClick);
+}
 
 // hide the menu when a click event occurs outside the menu
-document.addEventListener('click', (event) => {
-  if (!mainModalMenu.contains(event.target) && !mainModalMenuOpenButton.contains(event.target)) {
-    mainModalMenu.classList.remove('is-open2');
+document.addEventListener('click', event => {
+  if (
+    !mainMenu.contains(event.target) &&
+    !mainModalMenuOpenButton.contains(event.target)
+  ) {
+    console.log('clicked outside menu');
+    remove();
+    console.log(menu);
   }
 });
