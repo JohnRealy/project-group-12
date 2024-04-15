2d03e893fcfc8bc14c3b08824a3d58473fbc4a38
Anton Postnov <4qc7@proton.me>
2024-04-15 14:02:24 +0300

    main modal menu ADDED

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,6 +1,8 @@
 const menuCloseButton = document.getElementById('mobile-menu-close');
 const menuOpenButton = document.getElementById('mobile-menu-open');
+const mainModalMenuOpenButton = document.getElementById('main-modal-menu-open');
 const menu = document.getElementById('mobile-menu-id');
+const mainModalMenu = document.getElementById('main-modal-menu-id');
 const body = document.getElementsByTagName('body');
 
 menuOpenButton.addEventListener('click', () => {
@@ -13,4 +15,21 @@ menuCloseButton.addEventListener('click', () => {
   console.log("clicked close button");
   menu.classList.remove('is-open');
   body[0].classList.remove('no-scroll');
+});
+
+mainModalMenuOpenButton.addEventListener('click', () => {
+  console.log('clicked main menu button');
+  mainModalMenu.classList.add('is-open2');
+  body[0].classList.add('no-scroll');
+});
+
+// hide the menu when a click event occurs outside the menu
+document.addEventListener('click', (event) => {
+  if (!mainModalMenu.contains(event.target) && !mainModalMenuOpenButton.contains(event.target)) {
+    console.log("clicked outside menu");
+    mainModalMenu.classList.remove('is-open2');
+    body[0].classList.remove('no-scroll');
+    console.log(menu);
+    console.log(body);
+  }
 });
