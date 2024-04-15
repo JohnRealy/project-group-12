002fcd2bf592cd93436bdc3173b04e203ca4df94
Anton Postnov <4qc7@proton.me>
2024-04-15 22:09:17 +0300

    fixes
    1) remove is-open on click on links in mobile menu
    2) removed border on order-btn

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,19 +2,32 @@ const menuCloseButton = document.getElementById('mobile-menu-close');
 const menuOpenButton = document.getElementById('mobile-menu-open');
 const mainModalMenuOpenButton = document.getElementById('main-modal-menu-open');
 const menu = document.getElementById('mobile-menu-id');
-const menuLink = document.querySelectorAll('mobile-menu-nav-list-item');
+const menuLink = document.getElementsByClassName('mobile-menu-nav-list-item');
 const mainModalMenu = document.getElementById('main-modal-menu-id');
-const mainModalMenuLink = document.querySelectorAll('main-modal-list-item');
+const mainModalMenuLink = document.getElementsByClassName('main-modal-list-item');
 const body = document.getElementsByTagName('body');
 const mainMenu = document.querySelector('.main-modal-menu');
-
 
 // show/hide the menu when the button is clicked
 for (let link of menuLink) {
   // console.log(link);
   link.addEventListener('click', () => {
+    console.log("clicked link");
     menu.classList.remove('is-open');
     body[0].classList.remove('no-scroll');
+    console.log(menu);
+    console.log(body);
+  });
+}
+
+for (let link of mainModalMenuLink) {
+  // console.log(link);
+  link.addEventListener('click', () => {
+    console.log("clicked link");
+    mainMenu.classList.remove('is-open');
+    body[0].classList.remove('no-scroll');
+    console.log(menu);
+    console.log(body);
   });
 }
 
