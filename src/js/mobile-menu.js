0314e2338061572ee5ca6eeeda1fa91f79c27eca
Anton Postnov <4qc7@proton.me>
2024-04-15 16:08:51 +0300

    js update

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,8 +2,33 @@ const menuCloseButton = document.getElementById('mobile-menu-close');
 const menuOpenButton = document.getElementById('mobile-menu-open');
 const mainModalMenuOpenButton = document.getElementById('main-modal-menu-open');
 const menu = document.getElementById('mobile-menu-id');
+const menuLink = document.getElementsByClassName('mobile-menu-nav-list-item');
 const mainModalMenu = document.getElementById('main-modal-menu-id');
+const mainModalMenuLink = document.getElementById('main-modal-list-item');
 const body = document.getElementsByTagName('body');
+
+// show/hide the menu when the button is clicked
+for (let link of menuLink) {
+  // console.log(link);
+  link.addEventListener('click', () => {
+    console.log("clicked link");
+    menu.classList.remove('is-open');
+    body[0].classList.remove('no-scroll');
+    console.log(menu);
+    console.log(body);
+  });
+}
+
+for (let link of mainModalMenuLink) {
+  // console.log(link);
+  link.addEventListener('click', () => {
+    console.log("clicked link");
+    mainModalMenu.classList.remove('is-open');
+    body[0].classList.remove('no-scroll');
+    console.log(menu);
+    console.log(body);
+  });
+}
 
 menuOpenButton.addEventListener('click', () => {
   console.log('clicked open button');
