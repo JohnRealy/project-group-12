f9005c45fc0052a5ff48750999aa7123cab06486
balakun <balakun0505@gmail.com>
2024-04-17 15:48:32 +0100

    everything done

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,103 +1,114 @@
-// import Swiper from 'swiper';
-// import { getData } from './api';
-// import Swiper from './swiper';
-// import Swiper, { Navigation } from 'swiper';
-import Swiper from './swiper.js';
-import { getReviews } from './api.js';
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-// import { Navigation, Keyboard } from 'swiper/modules';
+import { getReviews } from './api';
 
-var swiper = new Swiper('.swiper-container', {
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+const list = document.querySelector('.reviews-list');
+const swiperContainer = document.querySelector('.reviews-swiper');
+const nextButtonRev = document.querySelector('.reviews-next-btn');
+const prevButtonRev = document.querySelector('.reviews-prev-btn');
+
+function renderReview({ avatar_url, author, review }) {
+  return `<li class="reviews-item swiper-slide">
+              <img class="reviews-item-img" src="${avatar_url}" alt="Ihor Trachuk" width="48" height="48" />
+              <h3 class="reviews-item-title">${author}</h3>
+              <p class="reviews-item-text">
+                ${review}
+              </p>
+          </li>`;
+}
+function renderReviews(reviewsArr) {
+  return reviewsArr.map(renderReview).join('');
+}
+
+function renderErrorText() {
+  const itemError = document.createElement('li');
+  const span = document.createElement('span');
+  itemError.classList.add('reviews-item-error');
+  span.textContent = 'Not found';
+  span.classList.add('reviews-text-error');
+  itemError.appendChild(span);
+  list.appendChild(itemError);
+}
+
+document.addEventListener('DOMContentLoaded', handleReviews);
+
+async function handleReviews() {
+  try {
+    const review = await getReviews();
+    const markup = renderReviews(review);
+    list.insertAdjacentHTML('beforeend', markup);
+  } catch (error) {
+    console.error('Error :', error);
+    renderErrorText();
+    iziToast.info({
+      close: false,
+      position: 'topRight',
+      message: 'Error!',
+    });
+  }
+}
+
+const swiper = new Swiper(swiperContainer, {
+  modules: [Navigation],
+  slidesPerView: 1,
+  spaceBetween: 16,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
   },
+
   breakpoints: {
-    375: {
-      slidesPerView: 1,
-    },
     768: {
       slidesPerView: 2,
-      spaceBetween: 16,
-    },
-    1440: {
-      slidesPerView: 4,
-      grabCursor: true,
       spaceBetween: 18,
     },
+
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 16,
+    },
+  },
+  direction: 'horizontal',
+  navigation: {
+    nextEl: '.reviews-next-btn',
+    prevEl: '.reviews-prev-btn',
   },
 });
-// API
 
-function createMarkup(arr) {
-  return arr
-    .map(
-      user => `<li class="reviews-item swiper-slide">
-          <img
-            class="reviews-item-img"
-            src="${user.avatar_url}"
-            alt="Ihor Trachuk"
-          />
-          <h3 class="reviews-item-name">${user.author}</h3>
-          <p class="reviews-text">
-            ${user.review}
-          </p>
-        </li>`
-    )
-    .join('');
-}
+const nextButtRevonObserver = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (mutation.attributeName === 'class') {
+      const disabled = nextButtonRev.classList.contains(
+        'swiper-button-disabled'
+      );
+      if (disabled) {
+        nextButtonRev.style.opacity = '0.5';
+      } else {
+        nextButtonRev.style.opacity = '1';
+      }
+    }
+  });
+});
 
-const reviewList = document.querySelector('.reviews-list');
+nextButtRevonObserver.observe(nextButtonRev, { attributes: true });
 
-document.addEventListener('DOMContentLoaded', renderPage);
+const prevButtRevonObserver = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (mutation.attributeName === 'class') {
+      const disabled = prevButtonRev.classList.contains(
+        'swiper-button-disabled'
+      );
+      if (disabled) {
+        prevButtonRev.style.opacity = '0.5';
+      } else {
+        prevButtonRev.style.opacity = '1';
+      }
+    }
+  });
+});
 
-async function renderPage() {
-  reviewList.insertAdjacentHTML('afterbegin', createMarkup(await getReviews()));
-}
-
-getReviews();
-// const slide = document.querySelector('.swiper').swiper;
-
-// Now you can use all slider methods like
-// slide.slideNext();
-
-// ==========================================================
-
-// const reviewsSwiper = new Swiper('.reviews-swiper', {
-//   modules: [Navigation, Keyboard],
-//   speed: 800,
-//   grabCursor: true,
-//   allowTouchMove: true,
-//   direction: 'horizontal',
-//   watchOverflow: true,
-//   spaceBetween: 16,
-
-//   navigation: {
-//     nextEl: '.next-review-btn',
-//     prevEl: '.prev-review-btn',
-//   },
-
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: true,
-//     pageUpDown: true,
-//   },
-
-//   breakpoints: {
-//     320: {
-//       slidesPerGroup: 1,
-//       slidesPerView: 1,
-//     },
-//     768: {
-//       slidesPerGroup: 2,
-//       slidesPerView: 2,
-//     },
-//     1440: {
-//       slidesPerGroup: 4,
-//       slidesPerView: 4,
-//     },
-//   },
-// });
-
-// =======================================================
+prevButtRevonObserver.observe(prevButtonRev, { attributes: true });
