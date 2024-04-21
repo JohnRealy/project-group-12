a14055f307c28877ac7b260ab1035253a375ac3e
VladyslavPiatak <vladislawatson@gmail.com>
2024-04-21 21:55:05 +0300

    reName

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,4 +1,4 @@
-const coversSection = document.querySelector('section-covers'); // Присвоєння змінної елементу з ID "section-covers"
+const coversSection = document.getElementById('section-covers'); // Присвоєння змінної елементу з ID "section-covers"
 const marqueeLines = document.querySelectorAll('.marquee__line'); // Вибір усіх елементів з класом "marquee__line"
 
 window.addEventListener('scroll', () => {
@@ -6,8 +6,10 @@ window.addEventListener('scroll', () => {
   const sectionTop = coversSection.offsetTop; // Отримання верхньої позиції розділу
 
   if (scrollPosition >= sectionTop - window.innerHeight / 2) {
-    marqueeLines.forEach(line => line.classList.add('animation')); // Додавання класу "animated" до кожного елемента "marquee__line"
+    marqueeLines.forEach(line => line.classList.add('animated')); // Додавання класу "animated" до кожного елемента "marquee__line"
   } else {
-    marqueeLines.forEach(line => line.classList.remove('animation')); // Видалення класу "animated" з кожного елемента "marquee__line"
+    marqueeLines.forEach(line => line.classList.remove('animated')); // Видалення класу "animated" з кожного елемента "marquee__line"
   }
 });
+
+
